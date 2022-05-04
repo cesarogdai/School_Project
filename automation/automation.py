7ef7a56a8e2bf517709389ccf43f140b05764a4c
cesarogdai <cesarogdai01@gmail.com>
2022-05-03 21:40:51 -0500

    Dropdown menu
    Added menu in home page

--- automation/automation.py
+++ automation/automation.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
 
-firefox_browser = webdriver.Firefox('./geckodriver')
+chrome_browser = webdriver.Chrome('./chromedriver')
 
-firefox_browser.maximize_window()
+chrome_browser.maximize_window()
 
-firefox_browser.get('http://127.0.0.1/School_Project')
+chrome_browser.get('http://127.0.0.1/School_Project')
 
-usernameInput = firefox_browser.find_element_by_id('username')
-user.clear()
-user.send_keys("A180410@UNACH.MX")
+username = chrome_browser.find_element_by_id('username')
+username.clear()
+username.send_keys("A180410@UNACH.MX")
 
-pswd= firefox_browser.find_element_by_id('password')
+password= chrome_browser.find_element_by_id('password')
 password.clear()
 password.send_keys("A180410")
 
-btn_login = firefox_browser.find_element_by_id('btnCheck')
+btn_login = chrome_browser.find_element_by_id('btnCheck')
 
 btn_login.click()
--- home.php
+++ home.php
@@ -6,10 +6,25 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="styles/home.css">
 	<title>HOME</title>
 </head>
 <body>
-	<h1>Welcome <?php echo $_SESSION['name']?></h1>
+	<div class="navbar">
+  <a href="#home">Home</a>
+  
+  <div class="dropdown">
+    <button class="dropbtn">Mas
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="kardex.php">Kardex</a>
+     
+    </div>
+  </div> 
+</div>
+
 
 </body>
 </html>
